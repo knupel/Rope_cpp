2634961734980901b77e370769a4531d5795702c
Stan le Punk <stanislasiii@gmail.com>
2020-08-09 16:56:33 +0200

    Update r_geom.hpp

--- rope/template/math/r_geom.hpp
+++ rope/template/math/r_geom.hpp
@@ -1,7 +1,7 @@
 /**
 * header
 * Rope math geom C++
-* v 0.0.4
+* v 0.0.5
 * 2020-2020
 * Rope C++ library adaptation in the same way of Rope from Processing
 * Rope mean ROmanesco Processing Environment at the beginning !!!
@@ -28,18 +28,43 @@
 */
 // https://en.wikipedia.org/wiki/List_of_common_coordinate_transformations#From_spherical_coordinates
 template<typename T>
+void coord_polar(T &x, T &y, T dir) {
+	x = sin(dir);
+	y = cos(dir);
+}
+
+template<typename T>
 void coord_polar(T &x, T &y, T dir, T radius) {
 	x = sin(dir) * radius;
 	y = cos(dir) * radius;
 }
 
 template<typename T>
+vec2<T> coord_polar(T dir) {
+	return vec2<T>(sin(dir),cos(dir));
+}
+
+
+template<typename T>
 vec2<T> coord_polar(T dir, T radius) {
 	return (vec2<T>(sin(dir),cos(dir)) * radius);
 }
 
-
 // https://fr.wikipedia.org/wiki/Coordonn%C3%A9es_polaires
+template<typename T>
+vec3<T> coord_sphere_math(T lat, T lon) {
+	double tau = 2.0 *M_PI;
+	double theta = fmod(lon,tau);
+	double phi = fmod(lat,tau);
+
+	T x = static_cast<T>(sin(theta) * cos(phi));
+	T y = static_cast<T>(sin(theta) * sin(phi));
+	T z = static_cast<T>(cos(theta));
+
+	return vec3<T>(x,y,z);
+}
+
+
 template<typename T>
 vec3<T> coord_sphere_math(T lat, T lon, T radius) {
 	double tau = 2.0 *M_PI;
@@ -57,15 +82,27 @@ vec3<T> coord_sphere_math(T lat, T lon, T radius) {
 /**
 * orthodromy, loxodromy is the science to compute distance, position on Sphere.
 * very very complexe.
+* and that's don't work for me.
 */
+// template<typename T>
+// T dist_orthodromy(T lat_1, T lon_1, T lat_2, T lon_2, T radius) {
+// 	T u;
+// 	T v;
+// 	u = sin((lat_2 - lat_1) * 0.5);
+// 	v = sin((lon_2 - lon_1) * 0.5);
+// 	return 2.0 * radius * asin(sqrt(u * u + cos(lat_1) * cos(lat_2) * v * v));
+// }
 template<typename T>
 T dist_orthodromy(T lat_1, T lon_1, T lat_2, T lon_2, T radius) {
-	T u;
-	T v;
-	u = sin((lat_2 - lat_1) * 0.5);
-	v = sin((lon_2 - lon_1) * 0.5);
-	return 2.0 * radius * asin(sqrt(u * u + cos(lat_1) * cos(lat_2) * v * v));
+	vec3<T> a = coord_sphere_math(lat_1, lon_1);
+	vec3<T> b = coord_sphere_math(lat_2, lon_2);
+	// T sum_product = (a.x() * b.x()) + (a.y() * b.y()) + (a.z() * b.z());
+	T sum_product = a.cross(b).sum();
+	return radius * acos(sum_product / (radius *radius));
+	// return acos((a *b) / (radius *radius));
+	//return asin((a *b) / (radius *radius));
 }
+
 
 template<typename T>
 T dist_orthodromy(vec2<T> a, vec2<T> b, T radius) {

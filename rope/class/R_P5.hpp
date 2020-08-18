6e83378e340c34d039dc52f526b58000fafde3ba
Stan le Punk <stanislasiii@gmail.com>
2020-08-18 17:41:32 +0200

    modify R_DaP5 struct to R_P5
    but there is no reason this file are in Rope, the better place maybe is in ofRope

--- rope/class/R_P5.hpp
+++ rope/class/R_P5.hpp
@@ -1,7 +1,14 @@
-struct R_DaP5 {
+/**
+* R_P5
+* 2020-2020
+* v 0.0.2
+*/
+
+struct R_P5 {
 	float width;
 	float height;
 	float mouse_x;
 	float mouse_y;
 	bool mouse_pressed;
+	int frame_count;
 };
